82bc04b466388729b0ae657cc31d4f132919f4ce
Douglas Francis <dougiefrancis@gmail.com>
2020-06-08 18:18:29 +0100

    App: Create App base component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
+  const [Todos, SetToDo] = useState([
+    {Text: 'Task 1'},
+    {Text: 'Task 2'},
+    {Text: 'Task 3'}]
+  )
+
+const ToDo = ({Todo, index}) => {
+return <div className='to-do'>
+        {Todo.Text}
+      </div>
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <div className='list' > {Todos.map((Todo, index) => <ToDo key={index} index={index} Todo={Todo}/>)}</div>
+
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
